32907598a435053c713a9b722137aa7031d5ca5f
Curtis Delicata <curtis@modulargaming.com>
2013-07-29 11:19:44 +0100

    Docblocks

--- classes/MG/Controller/Admin/Dashboard.php
+++ classes/MG/Controller/Admin/Dashboard.php
@@ -1,5 +1,13 @@
 <?php defined('SYSPATH') OR die('No direct script access.');
-
+/**
+ * View for admin dashboard page.
+ *
+ * @package    MG/Admin
+ * @category   Controller
+ * @author     Modular Gaming Team
+ * @copyright  (c) 2012-2013 Modular Gaming Team
+ * @license    BSD http://modulargaming.com/license
+ */
 class MG_Controller_Admin_Dashboard extends Abstract_Controller_Admin {
 
 	/**
--- classes/MG/View/Admin/Dashboard.php
+++ classes/MG/View/Admin/Dashboard.php
@@ -1,5 +1,13 @@
 <?php defined('SYSPATH') OR die('No direct script access.');
-
+/**
+ * View for admin dashboard.
+ *
+ * @package    MG/Admin
+ * @category   View
+ * @author     Modular Gaming Team
+ * @copyright  (c) 2012-2013 Modular Gaming Team
+ * @license    BSD http://modulargaming.com/license
+ */
 class MG_View_Admin_Dashboard extends Abstract_View_Admin {
 
 	public $title = 'Dashboard';
